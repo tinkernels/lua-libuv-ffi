6375d3742c4b20089324bb355263bd238f726ae0
tinkernels <don.johnny.cn@gmail.com>
2023-01-11 02:12:08 +0800

    Modify windows build scripts.

--- lua-libuv-ffi/build-win32.cmd
+++ lua-libuv-ffi/build-win32.cmd
@@ -3,6 +3,8 @@ chcp 65001 >NUL 2>NUL
 
 cd %~dp0
 
+echo working dir %~dp0
+
 call %~dp0vsenv.cmd 32
 
 cmake -DLIBUV_BUILD_SHARED=ON -DLIBUV_BUILD_TESTS=OFF -DLIBUV_BUILD_BENCH=OFF -DCMAKE_BUILD_TYPE=Release -DUV_FFI_WIN_DLL_NAME=libuv-ffi-win32 -S%~dp0.. -B%~dp0..\build
--- lua-libuv-ffi/build-winx64.cmd
+++ lua-libuv-ffi/build-winx64.cmd
@@ -3,6 +3,8 @@ chcp 65001 >NUL 2>NUL
 
 cd %~dp0
 
+echo working dir %~dp0
+
 call %~dp0vsenv.cmd 64
 
 cmake -DLIBUV_BUILD_SHARED=ON -DLIBUV_BUILD_TESTS=OFF -DLIBUV_BUILD_BENCH=OFF -DCMAKE_BUILD_TYPE=Release -DUV_FFI_WIN_DLL_NAME=libuv-ffi-winx64 -S%~dp0.. -B%~dp0..\build
